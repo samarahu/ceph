cd8006abe36118489a2f5d3fbb4a04f8268febaa
Samarah <samarah.uriarte@ibm.com>
2024-04-12 20:56:45 +0000

    rgw/test: Update RedisDriver and D4NPolicy unit tests
    Signed-off-by: Samarah <samarah.uriarte@ibm.com>

--- src/test/rgw/test_d4n_policy.cc
+++ src/test/rgw/test_d4n_policy.cc
@@ -73,6 +73,7 @@ class LFUDAPolicyFixture : public ::testing::Test {
       ASSERT_NE(conn, nullptr);
 
       dir->init(env->cct);
+      env->cct->_conf->rgw_local_cache_address = "127.0.0.1:6379";
       cacheDriver->initialize(env->dpp);
 
       bl.append("test data");
@@ -250,7 +251,7 @@ TEST_F(LFUDAPolicyFixture, RemoteGetBlockYield)
     conn->cancel();
   });
 
-  io.run();
+  io.run(); 
 }
 
 TEST_F(LFUDAPolicyFixture, BackendGetBlockYield)
