3ca5ca3b38670a21d68db5bf4a329c8b92cb41a7
Pritha Srivastava <prsrivas@redhat.com>
2024-09-11 14:55:48 +0530

    rgw/d4n: correcting the key used while updating the LFUDA data structure for the head object block in the read path.
    Signed-off-by: Pritha Srivastava <prsrivas@redhat.com>

--- src/rgw/driver/d4n/rgw_sal_d4n.cc
+++ src/rgw/driver/d4n/rgw_sal_d4n.cc
@@ -751,7 +751,11 @@ bool D4NFilterObject::check_head_exists_in_cache_get_oid(const DoutPrefixProvide
       found_in_cache = false;
       ldpp_dout(dpp, 10) << "D4NFilterObject::" << __func__ << "(): CacheDriver get_attrs method failed." << dendl;
     }
-    std::string key = head_oid_in_cache.erase(0, 2); // Remove dirty prefix
+    std::string key = head_oid_in_cache;
+    if (block.cacheObj.dirty) {
+      // Remove dirty prefix
+      key = key.erase(0, 2);
+    }
     this->driver->get_policy_driver()->get_cache_policy()->update(dpp, key, 0, 0, version, block.cacheObj.dirty, y);
   } else if (ret == -ENOENT) { //if blockDir->get
     found_in_cache = false;
