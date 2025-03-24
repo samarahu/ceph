955f10e7fbf2aa34648cd34c7ef2554a1b7b53bd
Samarah <samarah.uriarte@ibm.com>
2025-03-24 15:30:12 +0000

    d4n/directory: line 1181: Log value change
    Signed-off-by: Samarah <samarah.uriarte@ibm.com>

--- src/rgw/driver/d4n/d4n_directory.cc
+++ src/rgw/driver/d4n/d4n_directory.cc
@@ -1178,7 +1178,7 @@ int BlockDirectory::remove_host(const DoutPrefixProvider* dpp, CacheBlock* block
       }
 
       if (std::get<0>(resp).value().value().empty()) {
-	ldpp_dout(dpp, 0) << "BlockDirectory::" << __func__ << "(): No values returned." << dendl;
+	ldpp_dout(dpp, 10) << "BlockDirectory::" << __func__ << "(): No values returned." << dendl;
 	return -ENOENT;
       }
 
