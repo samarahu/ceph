bedfdd3fafd5f3805bd70a4d5dc6fe5c03bcaa8c
Samarah <samarah.uriarte@ibm.com>
2025-03-24 15:41:53 +0000

    d4n/directory: line 536: Log value change
    Signed-off-by: Samarah <samarah.uriarte@ibm.com>

--- src/rgw/driver/d4n/d4n_directory.cc
+++ src/rgw/driver/d4n/d4n_directory.cc
@@ -533,7 +533,7 @@ int ObjectDirectory::zadd(const DoutPrefixProvider* dpp, CacheObj* object, doubl
 
     if (!multi) {
       if (std::get<0>(resp).value() != "1") {
-        ldpp_dout(dpp, 0) << "ObjectDirectory::" << __func__ << "() Response value is: " << std::get<0>(resp).value() << dendl;
+        ldpp_dout(dpp, 10) << "ObjectDirectory::" << __func__ << "() Response value is: " << std::get<0>(resp).value() << dendl;
         return -ENOENT;
       }
     }
