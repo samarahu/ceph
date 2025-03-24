3b8414bb85feb9e408be7830864758639cec0649
Samarah <samarah.uriarte@ibm.com>
2025-03-24 15:51:08 +0000

    d4n/directory: line 91: Log value change
    Signed-off-by: Samarah <samarah.uriarte@ibm.com>

--- src/rgw/driver/d4n/d4n_directory.cc
+++ src/rgw/driver/d4n/d4n_directory.cc
@@ -88,7 +88,7 @@ int BucketDirectory::zadd(const DoutPrefixProvider* dpp, const std::string& buck
 
     if (!multi) {
       if (std::get<0>(resp).value() != "1") {
-        ldpp_dout(dpp, 0) << "BucketDirectory::" << __func__ << "() Response value is: " << std::get<0>(resp).value() << dendl;
+        ldpp_dout(dpp, 10) << "BucketDirectory::" << __func__ << "() Response value is: " << std::get<0>(resp).value() << dendl;
         return -ENOENT;
       }
     }
